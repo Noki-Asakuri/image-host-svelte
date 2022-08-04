e99a7356491204a5df245f079f7b978e971902a6
Freundschaft <phungtanphat23@gmail.com>
2022-08-05 01:55:08 +0700

    First commit.

---
+++ src/routes/[imageID]/index.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+    interface image {
+        name: string;
+        author: string;
+        publicUrl: string;
+    }
+
+    export let image: image;
+</script>
+
+<svelte:head>
+    <title>Image Host</title>
+    <meta name="title" content="ShareX Image Host" />
+    <meta name="author" content="Asakuri#8323" />
+    <meta name="theme-color" content="#8e7489" />
+    <meta name="description" content="Image Host by Asakuri#8323" />
+
+    <meta content={image.name} property="og:title" />
+    <meta content={image.author} property="og:site_name" />
+    <meta content={image.publicUrl} property="og:image" />
+    <meta name="twitter:card" content="summary_large_image" />
+</svelte:head>
+
+<div class="relative flex items-center justify-center w-screen h-screen">
+    <div
+        class="relative flex justify-center items-center m-2 max-w-[80vw] max-h-[80vh]"
+    >
+        <!-- svelte-ignore a11y-img-redundant-alt -->
+        <img
+            alt={image.name}
+            src={image.publicUrl}
+            class="border shadow-lg h-max w-max shadow-slate-500 filter-none border-slate-600"
+        />
+    </div>
+</div>
---
+++ src/routes/[imageID]/index.ts
@@ -0,0 +1,18 @@
+import { prisma } from "../../server/db/client";
+import type { RequestHandler } from "./__types";
+
+export const GET: RequestHandler = async ({ params }) => {
+    const image = await prisma.image.findFirst({
+        where: { invisibleID: params.imageID },
+        select: { publicUrl: true, author: true, name: true },
+    });
+
+    if (!image) {
+        return { status: 404, body: { message: "Image not found!" } };
+    }
+
+    return {
+        status: 200,
+        body: { image },
+    };
+};
