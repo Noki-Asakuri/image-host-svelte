92df5a7bb1d4774ad33916eed7b56516e554d174
Freundschaft <phungtanphat23@gmail.com>
2022-08-07 10:25:02 +0700

    Update supabaseUrl.

--- src/lib/server/supabase.ts
+++ src/lib/server/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 import { env } from "$env/dynamic/private";
 
-const supabaseUrl = "https://vhkawzjiqyrfmnfllexh.supabase.co";
+const supabaseUrl = "https://etlzhwvhgxauolvcnmji.supabase.co";
 
 export const supabase = createClient(supabaseUrl, env.SUPABASE_KEY).storage.from("images");
