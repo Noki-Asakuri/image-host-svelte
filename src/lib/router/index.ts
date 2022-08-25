af92619712b9f970e7957084bf625d95d768b980
Asakuri Noki <phungtanphat23@gmail.com>
2022-08-25 22:09:47 +0700

    New router for image.

--- src/lib/router/index.ts
+++ src/lib/router/index.ts
@@ -3,10 +3,12 @@ import { createRouter } from "./context";
 export { createContext } from "./context";
 
 import { keyRouter } from "./sub/key";
+import { imageRouter } from "./sub/image";
 
 export const router = createRouter()
     .formatError(({ shape }) => shape)
-    .merge("key.", keyRouter);
+    .merge("key.", keyRouter)
+    .merge("image.", imageRouter);
 
 // export type definition of API
 export type Router = typeof router;
