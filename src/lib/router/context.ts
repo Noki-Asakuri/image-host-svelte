b9b938ca86ebe1f369ff33539f7fe1a7b30f592d
Asakuri Noki <phungtanphat23@gmail.com>
2022-08-25 22:09:32 +0700

    Add supabase to trpc context.

--- src/lib/router/context.ts
+++ src/lib/router/context.ts
@@ -1,7 +1,8 @@
 import * as trpc from "@trpc/server";
 import { prisma } from "$lib/server/prisma";
+import { supabase } from "$lib/server/supabase";
 
-export const createContext = async () => ({ prisma });
+export const createContext = async () => ({ prisma, supabase });
 
 type Context = trpc.inferAsyncReturnType<typeof createContext>;
 export const createRouter = () => trpc.router<Context>();
